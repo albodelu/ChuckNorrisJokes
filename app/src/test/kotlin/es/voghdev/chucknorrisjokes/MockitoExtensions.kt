dd26f97a6a79438d50768ae54bac383f51957876
Olmo Gallegos <olmo@appandweb.es>
2018-08-29 11:59:53 +0200

    Add KotlinTest dep. Reduce a good number of warnings

--- app/src/test/kotlin/es/voghdev/chucknorrisjokes/MockitoExtensions.kt
+++ app/src/test/kotlin/es/voghdev/chucknorrisjokes/MockitoExtensions.kt
@@ -2,16 +2,13 @@ package es.voghdev.chucknorrisjokes
 
 import com.nhaarman.mockito_kotlin.whenever
 import es.voghdev.chucknorrisjokes.app.ResLocator
+import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito
 
 fun ResLocator.hasString(id: Int, str: String) {
     whenever(getString(id)).thenReturn(str)
 }
 
-fun <T> anyCategory(): T {
-    return Mockito.anyObject<T>()
-}
+fun <T> anyCategory(): T = any()
 
-fun <T> anyJoke(): T {
-    return Mockito.anyObject<T>()
-}
+fun <T> anyJoke(): T = any()
